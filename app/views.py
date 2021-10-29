59f858963d05a826eb95ead3817b175628d7b606
devseme <ian.ochenge@student.moringaschool.com>
2021-10-29 18:23:56 +0300

    did the control-flow html pages

--- app/views.py
+++ app/views.py
@@ -19,3 +19,12 @@ def movie(news_id):
     View movie page function that returns the movie details page and its data
     '''
     return render_template('news.html',id = news_id)    
+
+def index():
+
+    '''
+    View root page function that returns the index page and its data
+    '''
+
+    title = 'Home - Welcome to The best News Review Website Online'
+    return render_template('index.html', title = title)    
